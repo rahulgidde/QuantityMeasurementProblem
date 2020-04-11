20d668f339fe6f3b23440fa050b53ada193af3e5
rahulgidde <rahulgidde1212@gmail.com>
2020-04-11 13:33:23 +0530

    Added:Test Case For Type Check

--- src/main/java/service/QuantityMeasurement.java
+++ src/main/java/service/QuantityMeasurement.java
@@ -22,4 +22,16 @@ public class QuantityMeasurement {
         double result = adapter.compare(value);
         return result;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null)
+            return false;
+        else if (object == this)
+            return true;
+        else if (object.getClass() == this.getClass())
+            return true;
+        else
+            return super.equals(object);
+    }
 }
---
+++ src/test/java/QuantityMeasurementTest.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Test;
+import service.QuantityMeasurement;
+
+public class QuantityMeasurementTest {
+    boolean result;
+
+    @Test
+    public void givenZeroFeetAndZeroFeet_WhenEqual_ShouldReturnTrue() {
+        QuantityMeasurement measurement = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
+        double firstConversion = measurement.getConversion(0.0);
+        QuantityMeasurement measurement1 = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
+        double secondConversion = measurement1.getConversion(0.0);
+        Assert.assertEquals(firstConversion, secondConversion, 0.0);
+    }
+
+    @Test
+    public void givenZeroFeetAndZeroFeet_WhenNullCheck_ShouldReturnResult() {
+        QuantityMeasurement measurement = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
+        double firstConversion = measurement.getConversion(0.0);
+        result = measurement.equals(null);
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenZeroFeetAndZeroFeet_WhenCheckReference_ShouldReturnResult() {
+        QuantityMeasurement measurement = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
+        double firstConversion = measurement.getConversion(0.0);
+        result = measurement.equals(measurement);
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenConversionType_WhenCheckType_ShouldReturnResult() {
+        QuantityMeasurement measurement = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
+        QuantityMeasurement measurement1 = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
+        result = measurement.equals(measurement1);
+        Assert.assertEquals(true, result);
+    }
+}
