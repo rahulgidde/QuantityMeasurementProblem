1a8532b9f1b5fc5d27be6f0efcc4bc2ff011fdf9
rahulgidde <rahulgidde1212@gmail.com>
2020-04-11 12:40:39 +0530

    Modified:Changes Class Name

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,20 +1,21 @@
 import org.junit.Assert;
 import org.junit.Test;
+import service.QuantityMeasurement;
 
-public class QuantityMeasurement {
+public class QuantityMeasurementTest {
 
     @Test
     public void givenZeroFeetAndZeroFeet_WhenEqual_ShouldReturnTrue() {
-        service.QuantityMeasurement measurement = new service.QuantityMeasurement(service.QuantityMeasurement.Conversion.FEET_TO_INCH);
+        QuantityMeasurement measurement = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
         double firstConversion = measurement.getConversion(0.0);
-        service.QuantityMeasurement measurement1 = new service.QuantityMeasurement(service.QuantityMeasurement.Conversion.FEET_TO_INCH);
+        QuantityMeasurement measurement1 = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
         double secondConversion = measurement1.getConversion(0.0);
         Assert.assertEquals(firstConversion, secondConversion, 0.0);
     }
 
     @Test
     public void givenZeroFeetAndZeroFeet_WhenNullCheck_ShouldReturnResult() {
-        service.QuantityMeasurement measurement = new service.QuantityMeasurement(service.QuantityMeasurement.Conversion.FEET_TO_INCH);
+        QuantityMeasurement measurement = new QuantityMeasurement(QuantityMeasurement.Conversion.FEET_TO_INCH);
         double firstConversion = measurement.getConversion(0.0);
         boolean result = measurement.equals(null);
         Assert.assertEquals(false, result);
